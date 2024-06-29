8e3bb57692ccf7b694d7431d8a434ba67e336981
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-06-30 00:59:06 +0900

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,37 @@
 import { footerText } from "@/constants"
 
 export default function Footer() {
-  return (
-    <footer id="footer" role="contentinfo">
-        <div class="footer__inner">
-            <div class="footer__text">
-                <span>gnlgk</span>
-                <span>© gnlgk</span>
-            </div>
-            <div class="footer__info">
-                <div class="left">
-                    <div class="title">
-                        <a href="#">sign up</a>
+    return (
+        <footer id="footer" role="contentinfo">
+            <div class="footer__inner">
+                <div class="footer__text">
+                    <span>gnlgk</span>
+                    <span>© gnlgk</span>
+                </div>
+                <div class="footer__info">
+                    <div class="left">
+                        <div class="title">
+                            <a href="#">sign up</a>
+                        </div>
+                        <p>회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
                     </div>
-                    <p>회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+                    <div class="right">
+                        <h3>social</h3>
+                        <ul>
+                            {footerText.map((footer, key) => (
+                                <li key={key}>
+                                    <a href={footer.link}>{footer.title}</a>
+                                    <em>{footer.desc}</em>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
-                <div class="right">
-                    <h3>social</h3>
-                    <ul>
-                    {footerText.map((footer,key)=>(
-                        <li key={key}>
-                            <a href={footer.link}>{ footer.title }</a>
-                            <em>{ footer.desc }</em>
-                        </li>
-                    ))}
-                    </ul>
+                <div class="footer__right">
+                    © 2023 gnlgk<br />
+                    이 사이트는 넥스트를 이용하여 제작하였습니다.
                 </div>
             </div>
-            <div class="footer__right">
-                © 2023 gnlgk<br />
-                이 사이트는 뷰를 이용하여 제작하였습니다.
-            </div>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
